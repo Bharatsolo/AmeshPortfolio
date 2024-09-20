36506e0742609379f1b8be96229d96a61ab885c5
Kamrajugadda sai Bharadwaj <148251061+Bharatsolo@users.noreply.github.com>
2024-09-20 12:29:35 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents actual form submission for this demo
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
